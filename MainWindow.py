6a663cfae55850c1d6aaee1aa9e4b9de4b62775f
raydialow <raydialow@protonmail.com>
2019-08-03 22:22:32 -0700

    modified:   MainWindow.py 	modified:   freeWrite.py 	modified:   mainwindow.ui

--- MainWindow.py
+++ MainWindow.py
@@ -51,4 +51,4 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "freeWrite"))
